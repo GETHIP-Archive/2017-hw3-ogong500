3b5987774f654e8cafe7c6087b2b327ad5275329
Owen Gong <ogong500@millardps.org>
2016-10-14 19:39:42 -0500

    Test

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
@@ -10,12 +11,22 @@ Template.body.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
 });
 
+
 Template.body.helpers({
   tasks() {
-    // Show newest tasks at the top
-   return Tasks.find({}, { sort: { createdAt: -1 } });
- },
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  },
 });
+
 Template.body.events({
  'submit .new-task'(event) {
    // Prevent default browser form submit
@@ -27,11 +38,16 @@ Template.body.events({
 
    // Insert a task into the collection
    Tasks.insert({
-     text,
-     createdAt: new Date(), // current time
-   });
+   text,
+   createdAt: new Date(), // current time
+   owner: Meteor.userId(),
+   username: Meteor.user().username,
+ });
 
-   // Clear form
+ // Clear form
    target.text.value = '';
+ },
+ 'change .hide-completed input'(event, instance) {
+   instance.state.set('hideCompleted', event.target.checked);
  },
 });
